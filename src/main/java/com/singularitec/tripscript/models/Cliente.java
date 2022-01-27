95b539d5e6855e3a87a4ba0e8f7ae37161978d6e
CJBiohacker <cj.moyses@gmail.com>
2022-01-26 23:23:20 -0300

    "fix: correção falta de especificação da versão maven"

--- src/main/java/com/singularitec/tripscript/models/Cliente.java
+++ src/main/java/com/singularitec/tripscript/models/Cliente.java
@@ -1,4 +1,16 @@
 package com.singularitec.tripscript.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity                                                         // Entidade de Banco de Dados SQL
 public class Cliente {
+
+    @Id                                                         // Define como Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)         // Ativa AutoIncrement
+    private int idCliente;
+    private String nome;
+    
 }
