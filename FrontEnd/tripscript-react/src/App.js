7e0662117156ce678f6a480cb2d72bffe1c06dfd
CJBiohacker <cj.moyses@gmail.com>
2022-02-06 01:13:36 -0300

    "chore : HeaderAppBar e pagina index"

--- FrontEnd/tripscript-react/src/App.js
+++ FrontEnd/tripscript-react/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import ResponsiveAppBar from './components/ResponsiveAppBar'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './styles/global.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="">TRIPSCRIPT BITCH</h1>
-
+      <ResponsiveAppBar />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/:id" exact component={Room}
+          />
+          <Route path="/admin/rooms/:id" exact component={AdminRoom} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ FrontEnd/tripscript-react/src/components/Carousel.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+const Carousel = () => {
+
+    return (
+        <figure>
+            <div className="glide" id="options-type">
+                <div className="glide__track" data-glide-el="track">
+                    <ul className="glide__slides" id="slide-container">
+                        <li className="glide__slide"><img src="https://raw.githubusercontent.com/CJBiohacker/TripScript-Viagens/dc41b307bb886fb030b63c64571ca212db18edb6/images/fernando-de-noronha.svg" alt="" /></li>
+                        <li className="glide__slide"><img src="https://raw.githubusercontent.com/CJBiohacker/TripScript-Viagens/dc41b307bb886fb030b63c64571ca212db18edb6/images/gruta-lagoazul.svg" alt="" /></li>
+                        <li className="glide__slide"><img src="https://raw.githubusercontent.com/CJBiohacker/TripScript-Viagens/dc41b307bb886fb030b63c64571ca212db18edb6/images/penedo.svg" alt="" /></li>
+                        <li className="glide__slide"><img src="https://raw.githubusercontent.com/CJBiohacker/TripScript-Viagens/dc41b307bb886fb030b63c64571ca212db18edb6/images/porto-de-galinhas.svg" alt="" /></li>
+                        <li className="glide__slide"><img src="https://raw.githubusercontent.com/CJBiohacker/TripScript-Viagens/dc41b307bb886fb030b63c64571ca212db18edb6/images/praia-camburi-vitoriaes.svg" alt="" /></li>
+                    </ul>
+                    <div className="glide__arrows" data-glide-el="controls">
+                        <button className="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+                        <button className="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+                    </div>
+                </div>
+            </div>
+        </figure>
+    );
+};
+
+export default Carousel;
