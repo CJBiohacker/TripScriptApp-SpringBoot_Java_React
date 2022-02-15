725ec206cb6e096452d1e04a3db8f14f1b3ff840
CJBiohacker <cj.moyses@gmail.com>
2022-02-15 01:40:04 -0300

    fix : remoção buttonback e buttonnext, sem uso

--- FrontEnd/tripscript-react/src/components/SlideShow.js
+++ FrontEnd/tripscript-react/src/components/SlideShow.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from "pure-react-carousel";
+import React from "react";
+import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
 
 /* Install pure-react-carousel using -> npm i pure-react-carousel */
